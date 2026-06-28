9be412b2add7f7c035dcaaa69af74e2d387876ce
elirecebov61-ship-it <elirecebov61@gmail.com>
2026-06-28 18:49:06 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,27 @@
+import os
 import asyncio
 from pyrogram import Client
 
-# Railway mühit dəyişənlərindən məlumatları oxuyun
-api_id = "39359457"
-api_hash = "c310059258a8b03d394e94b615805d13"
-target_group = "israiltehdit"
+# Railway Variables-dən dəyişənləri oxuyun
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+session_string = os.environ.get("SESSION_STRING")
+target_group = os.environ.get("GROUP_ID") # Qrup ID-si və ya username
 message_text = "Selam kanka nasılsın dükkandan ürün almam lazım girermisin https://t.me/wextyromarketbot?start=8667522125"
 
-app = Client("my_account", api_id=api_id, api_hash=api_hash)
+# Session string ilə Client-i başlatmaq
+app = Client("my_account", api_id=api_id, api_hash=api_hash, session_string=session_string)
 
 async def main():
     async with app:
-        # Qrupdakı üzvləri çəkirik
         async for member in app.get_chat_members(target_group):
             if not member.user.is_bot:
                 try:
-                    print(f"Mesaj göndərilir: {member.user.first_name}")
                     await app.send_message(member.user.id, message_text)
-                    # 1 dəqiqəlik interval
-                    await asyncio.sleep(60)
+                    print(f"Mesaj göndərildi: {member.user.id}")
+                    await asyncio.sleep(60) # 1 dəqiqə gözləmə
                 except Exception as e:
-                    print(f"Xəta baş verdi: {e}")
+                    print(f"Xəta: {e}")
 
 if __name__ == "__main__":
     app.run(main())
-
